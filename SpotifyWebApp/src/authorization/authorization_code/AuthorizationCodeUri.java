3e190056d6bf55e1c86f0a418cf23281098829a8
Collin Kersten <cakerste@ncsu.edu>
2020-06-06 15:25:12 -0400

    Added documentation and a GUI

--- SpotifyWebApp/src/authorization/authorization_code/AuthorizationCodeUri.java
+++ SpotifyWebApp/src/authorization/authorization_code/AuthorizationCodeUri.java
@@ -4,6 +4,7 @@ import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.SpotifyHttpManager;
 import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import com.wrapper.spotify.model_objects.specification.Artist;
 import com.wrapper.spotify.model_objects.specification.PagingCursorbased;
 import com.wrapper.spotify.model_objects.specification.PlayHistory;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
@@ -20,52 +21,75 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.util.Scanner;
 
 import org.apache.hc.core5.http.ParseException;
 
+/**
+ * This program looks at your most recent 50 songs and gives you artist recommendations based on it
+ * @author Collin Kersten
+ *
+ */
 public class AuthorizationCodeUri {
-  private static final String clientId = "a6121e25ede04a19acd6d54a13253e66";
-  private static final String clientSecret = "5fef0a96b0f744aa8c00c2b1aa4bea55";
-  private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8888/callback");
-  public static final int LIMIT = 50;
-  public static final int RECS_PER_ARTIST = 3;
-  public static final int NUM_OF_ARTISTS = 3;
+	/** Change the first three variables to whatever you use in your project */
+    private static final String clientId = "a6121e25ede04a19acd6d54a13253e66";
+    private static final String clientSecret = "5fef0a96b0f744aa8c00c2b1aa4bea55";
+    private static final URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8888/callback");
+    /** the amount of songs to look at (Spotify API caps it at 50) */
+    public static final int LIMIT = 50;
+    /** the amount of recommendations per artist */
+    public static final int RECS_PER_ARTIST = 3;
+    /** Look at the top 3 artists for recommendations */
+    public static final int NUM_OF_ARTISTS = 3;
 
-  private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
-    .setClientId(clientId)
-    .setClientSecret(clientSecret)
-    .setRedirectUri(redirectUri)
-    .build();
-  private static final AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
+    /** builds the spotify API with a unique client id, client secret, and redirect URI */
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+      .setClientId(clientId)
+      .setClientSecret(clientSecret)
+      .setRedirectUri(redirectUri)
+      .build();
+    /** builds a URI for authorization */
+    private static final AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
           .state("x4xkmn9pu3j6ukrs8n")
           .scope("user-read-recently-played")
           .show_dialog(true)
     .build();
   
-  private static AuthorizationCodeRequest authorizationCodeRequest;
-
-  	public static void login() {
-	  	final URI uri = authorizationCodeUriRequest.execute();
-	  
-	  	try {
+    private static AuthorizationCodeRequest authorizationCodeRequest;
+    /**
+     * opens spotify
+     */
+    public static void openSpotify() {
+    	final URI uri = authorizationCodeUriRequest.execute();
+    	try {
 			java.awt.Desktop.getDesktop().browse(uri);
-			
-			Scanner scan = new Scanner(System.in);
-			System.out.println("Enter URL that you were redirected to: ");
-			String code = scan.next();
+		} catch (IOException e) {
+			System.out.println("Error opening link");
+		}
+    }
+    /**
+     * logs in to the api using the url that the user was directed to
+     * @param code url that the user was redirected to
+     * @return exit status, 0 for success 1 for failure
+     */
+  	public static int login(String code) {
+	  	try {
 			code = code.substring(36, code.length() - 25);
 			authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
-			scan.close();
 			AuthorizationCodeCredentials authCodeCredentials = authorizationCodeRequest.execute();
 			spotifyApi.setAccessToken(authCodeCredentials.getAccessToken());
 			spotifyApi.setRefreshToken(authCodeCredentials.getRefreshToken());
-		} catch (IOException | SpotifyWebApiException | ParseException e) {
-			System.out.println("Error logging in: " + e.getMessage());
+			return 0;
+		} catch (IOException | SpotifyWebApiException | ParseException | IndexOutOfBoundsException e) {
+			return 1;
 		}
   	}
-  	public static PagingCursorbased<PlayHistory> getRecentTracks() {
+  	/**
+  	 * gets the recent 50 tracks
+  	 * @return recent tracks in a list
+  	 */
+  	private static PagingCursorbased<PlayHistory> getRecentTracks() {
   		try {
 			return spotifyApi.getCurrentUsersRecentlyPlayedTracks().limit(LIMIT).build().execute();
 		} catch (ParseException | SpotifyWebApiException | IOException e) {
@@ -73,7 +97,12 @@ public class AuthorizationCodeUri {
 			return null;
 		}
   	}
-  	public static String getHTML(URL url) {
+  	/**
+  	 * gets the HTML page for the certain artist from www.music-map.com
+  	 * @param url to get the html page from
+  	 * @return html page
+  	 */
+  	private static String getHTML(URL url) {
   		try {
   			HttpURLConnection con = (HttpURLConnection)url.openConnection();
   			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
@@ -91,8 +120,12 @@ public class AuthorizationCodeUri {
   		}
   		
   	}
-  	public static void authorizationCodeUri_Sync() {
-	    PagingCursorbased<PlayHistory> recentTracks = getRecentTracks();
+  	/**
+  	 * Looks at the artists of the recent tracks and sorts them by most listened to artists first
+  	 * @return array of the artists names, sorted
+  	 */
+  	public static String[] getArtists() {
+  		PagingCursorbased<PlayHistory> recentTracks = getRecentTracks();
 		PlayHistory[] tracks = recentTracks.getItems();
 		List<RecentArtist> artists = new SinglyLinkedList<RecentArtist>();
 		for (int i = 0; i < LIMIT; i++) {
@@ -115,16 +148,51 @@ public class AuthorizationCodeUri {
 		
 		Sorter<RecentArtist> sorter = new MergeSorter<>();
 		sorter.sort(artistsArray);
+		String[] artistsNameArray = new String[NUM_OF_ARTISTS];
 		for (int i = 0; i < NUM_OF_ARTISTS; i++) {
-			URL url = createURL(artistsArray[i].getArtist());
+			artistsNameArray[i] = artistsArray[i].getArtist();
+		}
+		return artistsNameArray;
+  	}
+  	/**
+  	 * gets the artist recommendations in a string array
+  	 * @return artist recommendations
+  	 */
+  	public static String[] authorizationCodeUri_Sync() {
+  		String[] artistsArray = getArtists();
+  		String[] artistRecsArray = new String[RECS_PER_ARTIST * NUM_OF_ARTISTS];
+  		int j = 0;
+		for (int i = 0; i < NUM_OF_ARTISTS; i++) {
+			URL url = createURL(artistsArray[i]);
 			String content = getHTML(url);
 			List<String> artistRecs = parseHTML(content.toString());
+			
 			for (String artist : artistRecs) {
-				System.out.println(artist);
+				artistRecsArray[j++] = artist; 
 			}
+		}
+		return artistRecsArray;
+  	}
+  	
+  	/**
+  	 * opens up an artists profile on the spotify web player
+  	 * @param name of the artist
+  	 */
+  	public static void getArtistInfo(String name) {
+  		try {
+			Artist[] a = spotifyApi.searchArtists(name).build().execute().getItems();
+			URI url = new URI("https://open.spotify.com/artist/" + a[0].getId());
+			java.awt.Desktop.getDesktop().browse(url);
+		} catch (ParseException | SpotifyWebApiException | IOException | URISyntaxException e) {
+			System.out.println(e.getMessage());
 		}
   	}
   
+  	/**
+  	 * creates a URL by appending the artist name to the end of www.music-map.com
+  	 * @param artist to search
+  	 * @return newly created URL
+  	 */
   	private static URL createURL(String artist) {
   		StringBuffer str = new StringBuffer("https://www.music-map.com/");
 		for (int i = 0; i < artist.length(); i++) {
@@ -142,6 +210,11 @@ public class AuthorizationCodeUri {
 		}
 	}
   
+  	/**
+  	 * gets the artist recommendations from the HTML page
+  	 * @param html page to look at
+  	 * @return artist recommendations in a list
+  	 */
     private static List<String> parseHTML(String html) {
 	    List<String> artistRecs = new SinglyLinkedList<String>();
 	    for (int i = 1; i <= RECS_PER_ARTIST; i++) {
@@ -162,28 +235,52 @@ public class AuthorizationCodeUri {
 	    return artistRecs;
     }
   
+    /**
+     * Recent Artist class, used to keep track of the frequency, implements comparable so it is sortable
+     * @author Collin Kersten
+     *
+     */
   	private static class RecentArtist implements Comparable {
   		private String artist;
 	    private int frequency;
 	  
+	  /**
+	   * Constructor, sets the artist name and frequency
+	   * @param artist name
+	   * @param frequency of the artist
+	   */
 	  public RecentArtist(String artist, int frequency) {
 		  this.artist = artist;
 		  this.frequency = frequency;
 	  }
 	  
+	  /**
+	   * gets the artist
+	   * @return artist
+	   */
 	  public String getArtist() {
 		  return this.artist;
 	  }
-	  
+	  /**
+	   * increments the frequency
+	   */
 	  public void incrementFrequency() {
 		  this.frequency++;
 	  }
 	  
+	  /**
+	   * gets the frequency
+	   * @return frequency
+	   */
 	  public int getFrequency() {
 		  return this.frequency;
 	  }
 
-	  @Override
+	  /**
+	   * compares two recentartists
+	   * @param o other RecentArtist
+	   * @return compare value
+	   */
 	  public int compareTo(Object o) {
 		  RecentArtist oArtist = (RecentArtist)o;
 		  if (this.getFrequency() > oArtist.getFrequency()) {
@@ -195,8 +292,16 @@ public class AuthorizationCodeUri {
 	  }
 	  
     }
-  	public static void main(String[] args) {
-		login();
-	    authorizationCodeUri_Sync();
-	}
+  	
+  	/**
+  	 * gets the users display name
+  	 * @return display name
+  	 */
+  	public static String getUser() {
+  		try {
+			return spotifyApi.getCurrentUsersProfile().build().execute().getDisplayName();
+		} catch (ParseException | SpotifyWebApiException | IOException e) {
+			return null;
+		}
+  	}
 }
---
+++ SpotifyWebApp/src/view/ui/ArtistRecommenderGUI.java
@@ -0,0 +1,166 @@
+package view.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Line2D;
+
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import authorization.authorization_code.AuthorizationCodeUri;
+
+/**
+ * GUI class
+ * @author Collin Kersten
+ *
+ */
+public class ArtistRecommenderGUI implements ActionListener {
+	private JPanel panel;
+	private JLabel label;
+	private JTextArea ta;
+	private JScrollPane sp;
+	private JButton enter;
+	private JButton reset;
+	private JButton auth;
+	private JFrame frame;
+	private JButton getRecs;
+	private JList<String> recs;
+	private JList<String> recentArtists;
+	private JPanel panel1, panel2, panel3;
+	private JLabel user;
+	private JLabel artistHeader, recsHeader;
+	
+	/**
+	 * sets up the first screen, calls the login method
+	 */
+	public ArtistRecommenderGUI() {
+		frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        setUpLogin();
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.CENTER, auth);
+        frame.setVisible(true);
+	}
+	
+	/**
+	 * sets up the login screen
+	 */
+	private void setUpLogin() {
+		panel = new JPanel();
+        label = new JLabel("Enter URL");
+        ta = new JTextArea(1,20);
+        ta.setWrapStyleWord(true);
+        ta.setLineWrap(true);
+        sp = new JScrollPane(ta);
+        enter = new JButton("Enter");
+        enter.addActionListener(this);
+        reset = new JButton("Reset");
+        reset.addActionListener(this);
+        auth = new JButton("Get Authorization");
+        auth.addActionListener(this);
+        panel.add(label);
+        panel.add(sp);
+        panel.add(enter);
+        panel.add(reset);
+       
+	}
+	
+	/**
+	 * sets up the recommendation screen
+	 */
+	private void setUpRecs() {
+		panel1 = new JPanel();
+		panel2 = new JPanel();
+		panel2.setLayout(new BoxLayout(panel2, BoxLayout.PAGE_AXIS));
+		panel3 = new JPanel();
+		panel3.setLayout(new BoxLayout(panel3, BoxLayout.PAGE_AXIS));
+		getRecs = new JButton("Get Recommendations");
+		getRecs.addActionListener(this);
+		user = new JLabel("User: " + AuthorizationCodeUri.getUser());
+		artistHeader = new JLabel("Your most lsitened to artists recently");
+		recsHeader = new JLabel("Recommendations");
+		panel1.add(getRecs);
+		panel1.add(user);
+		user.setPreferredSize(new Dimension(300,10));
+		user.setHorizontalAlignment(SwingConstants.RIGHT);
+		frame.add(BorderLayout.PAGE_START, panel1);
+		
+		artistHeader = new JLabel("Your most listented to artists recently");
+		recentArtists = new JList<String>(AuthorizationCodeUri.getArtists());
+		recentArtists.setFixedCellHeight(145);
+		recentArtists.setBackground(frame.getBackground());
+		recentArtists.setPreferredSize(new Dimension(200, 400));
+		DefaultListCellRenderer renderer = (DefaultListCellRenderer)recentArtists.getCellRenderer();
+		renderer.setHorizontalAlignment(SwingConstants.CENTER);
+		panel2.add(artistHeader);
+		panel2.add(recentArtists);
+		recs = new JList<String>(AuthorizationCodeUri.authorizationCodeUri_Sync());
+		recs.setFixedCellHeight(recentArtists.getFixedCellHeight() / 3);
+		recs.setBackground(frame.getBackground());
+		panel3.add(recsHeader);
+		panel3.add(recs);
+		frame.getContentPane().add(BorderLayout.LINE_END, panel3);
+		frame.getContentPane().add(BorderLayout.LINE_START, panel2);
+		recs.setVisible(false);
+		recs.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent evt) {
+				if (evt.getClickCount() == 2) {
+					AuthorizationCodeUri.getArtistInfo(recs.getSelectedValue());
+				}
+			}
+		});
+		frame.validate();
+		frame.repaint();
+	}
+
+	/**
+	 * Listens for an action performed
+	 * @param e object that there is an action being performed
+	 */
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == reset) {
+			ta.setText("");
+		} else if (e.getSource() == enter) {
+			if (AuthorizationCodeUri.login(ta.getText()) == 0) {
+				frame.remove(panel);
+				frame.remove(auth);
+				setUpRecs();
+			} else {
+				JOptionPane.showMessageDialog(frame, "Incorrect URL");
+				ta.setText("");
+			}
+		} else if (e.getSource() == auth) {
+			AuthorizationCodeUri.openSpotify();
+		} else if (e.getSource() == getRecs) {
+			recs.setVisible(true);
+		}
+	}
+	
+	/**
+	 * creates a new GUI
+	 * @param args commnand-line arguments
+	 */
+	public static void main(String[] args) {
+		new ArtistRecommenderGUI();
+	}
+}
